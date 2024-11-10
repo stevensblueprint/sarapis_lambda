8eb5141b428770adf3d5ba1ac9220305cc04076a
miguel-merlin <mmerlin@stevens.edu>
2024-11-09 21:03:44 -0500

    feat: added exceptions

--- function/organization-handler-lambda/src/main/java/com/sarapis/service/OrganizationService.java
+++ function/organization-handler-lambda/src/main/java/com/sarapis/service/OrganizationService.java
@@ -1,15 +1,17 @@
 package com.sarapis.service;
 
+import com.sarapis.exceptions.OrganizationNotFoundException;
+import com.sarapis.exceptions.OrganizationServiceLogicException;
 import com.sarapis.model.Organization;
 import java.util.List;
 import java.util.UUID;
 
 public interface OrganizationService {
-    List<Organization> getAllOrganizations();
+    List<Organization> getAllOrganizations() throws OrganizationServiceLogicException;
 
-    Organization getOrganizationById(String id);
+    Organization getOrganizationById(String id) throws OrganizationNotFoundException, OrganizationServiceLogicException;
 
-    Organization createOrganization(Organization organization);
+    Organization createOrganization(Organization organization) throws OrganizationServiceLogicException;
 
     Organization updateOrganization(UUID id, Organization organization);
 
--- function/organization-handler-lambda/src/main/java/com/sarapis/service/OrganizationServiceImpl.java
+++ function/organization-handler-lambda/src/main/java/com/sarapis/service/OrganizationServiceImpl.java
@@ -2,12 +2,17 @@ package com.sarapis.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.sarapis.exceptions.OrganizationNotFoundException;
+import com.sarapis.exceptions.OrganizationServiceLogicException;
+import com.sarapis.lambda.OrganizationHandlerLambda;
 import com.sarapis.model.Organization;
 import java.util.List;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OrganizationServiceImpl implements OrganizationService {
-
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceImpl.class);
     private final DynamoDBMapper dynamoDBMapper;
 
     public OrganizationServiceImpl(DynamoDBMapper dynamoDBMapper) {
@@ -15,20 +20,47 @@ public class OrganizationServiceImpl implements OrganizationService {
     }
 
     @Override
-    public List<Organization> getAllOrganizations() {
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
-        return dynamoDBMapper.scan(Organization.class, scanExpression);
+    public List<Organization> getAllOrganizations() throws OrganizationServiceLogicException {
+        try {
+            DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+            return dynamoDBMapper.scan(Organization.class, scanExpression);
+        } catch (Exception e) {
+            logger.error("Failed to fetch all organizations", e);
+            throw new OrganizationServiceLogicException(e.getMessage());
+        }
     }
 
     @Override
-    public Organization getOrganizationById(String id) {
-        return dynamoDBMapper.load(Organization.class, id);
+    public Organization getOrganizationById(String id)
+            throws OrganizationNotFoundException, OrganizationServiceLogicException {
+        try {
+            Organization organization = dynamoDBMapper.load(Organization.class, id);
+            if (organization == null) {
+                logger.warn("Organization with ID= {} not found", id);
+                throw new OrganizationNotFoundException("Organization not found");
+            }
+            return organization;
+        } catch (OrganizationNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error("Failed to find organization with ID= {}", id, e);
+            throw new OrganizationServiceLogicException(e.getMessage());
+        }
     }
 
     @Override
-    public Organization createOrganization(Organization organization) {
-        dynamoDBMapper.save(organization);
-        return organization;
+    public Organization createOrganization(Organization organization)
+        throws OrganizationServiceLogicException {
+        try {
+            dynamoDBMapper.save(organization);
+            return organization;
+        } catch (IllegalArgumentException e) {
+            logger.error("Invalid argument provided while creating organization: {}", e.getMessage(), e);
+            throw new OrganizationServiceLogicException("Invalid argument provided: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("Failed to create organization", e);
+            throw new OrganizationServiceLogicException("An error occurred while creating the organization: " + e.getMessage());
+        }
     }
 
     @Override
